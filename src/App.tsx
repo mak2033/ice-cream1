c0d762de53d6dd28999061e407f5fef18f4e07cd
mak2033 <86602585+mak2033@users.noreply.github.com>
2026-03-16 01:43:37 +0300

    style: animate page transitions with smooth scroll

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,10 @@ const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
   }, [pathname]);
 
   return null;
